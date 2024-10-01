c9bdb314f876f5bd5ee1762395dfd0fcedd672e8
Aman Palya <amanpalya90@gmail.com>
2024-10-01 18:35:24 +0530

    this is my spring-boot -crud-project

---
+++ src/test/java/com/java/util/SpringBootCrudJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
